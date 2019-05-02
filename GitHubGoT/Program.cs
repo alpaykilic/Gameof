b31ba88480db08ecedbb7c115a1ad109f22c4a71
lab12 <lab12@CASPER_NIRVANA>
2019-05-02 11:54:07 +0300

    Jaime Ölmedi

--- GitHubGoT/Program.cs
+++ GitHubGoT/Program.cs
@@ -7,6 +7,7 @@ namespace GitHubGoT
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Ruhsuz Bran on his way");
         }
     }
 }
