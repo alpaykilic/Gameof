16638db1d82dff2294b27c0e6d317015abc21b24
lab12 <lab12@CASPER_NIRVANA>
2019-05-02 11:43:52 +0300

    Add project files.

---
+++ GitHubGoT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitHubGoT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
